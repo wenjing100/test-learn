8eb71d0914429d083436ab54a371f834d921ce1d
wenjing100 <55542032+wenjing100@users.noreply.github.com>
2021-04-13 20:53:06 +0800

    加载了lazyload但是没有用

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import wenjingUI from './libs'
+import vuelazyload from 'vue3-lazy'
 import { GET_CARDATA } from './store/actionTypes'
 
 import './assets/css/base.css'
@@ -11,6 +12,9 @@ import './assets/css/iconfont.css'
 
 const app = createApp(App);
 app.use(store).use(router).use(wenjingUI).mount('#app')
-
+app.use(vuelazyload,{
+    loading: 'loading.png',
+    error: 'error.png'
+  })
 //将购物车数据请求 并放到vuex中
 store.dispatch(GET_CARDATA,'');
